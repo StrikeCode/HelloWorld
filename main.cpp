7ac157355a1acc1bad8ffa30f442f840b56f1cf3
happyjyh <CodeTracker@outlook.com>
2023-05-29 14:31:37 +0800

    创建main.cpp,写了初始代码

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void sort(int arr[], int size)
+{
+    
+}
+
+int main()
+{
+    int arr[] = {11, 29, 8, 99, 2, 18};
+    int size = sizeof(arr) / sizeof(int);
+    sort(arr, size);
+    return 0;
+}
