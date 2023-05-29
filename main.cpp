6123e2ad4a7ea98e9f76e884fbb4b44baecfd6a1
StrikeCode <CodeTracker@outlook.com>
2023-05-29 14:44:55 +0800

    feat:实现sort函数

--- main.cpp
+++ main.cpp
@@ -4,13 +4,25 @@ using namespace std;
 
 void sort(int arr[], int size)
 {
-    
+    int tmp = 0; 
+    for(int i = 0; i < size - 1; ++i)
+    {
+        for(int j = 0; j < size - 1 - i; ++j)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
 
 int main()
 {
     int arr[] = {11, 29, 8, 99, 2, 18};
-    int size = sizeof(arr) / sizeof(int);
+    int size = sizeof(arr) / sizeof(arr[0]);
     sort(arr, size);
     return 0;
 }
